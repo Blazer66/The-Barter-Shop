8af54ff549ed62e9877f56bff1107f6cae0c29c9
Chrisite L Richardson <crichardson@catholiccharities.net>
2021-04-26 11:24:19 -0500

    Initial Commit

---
+++ The Barter Shop/The_Barter_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Barter_ShopApp.swift
+//  The Barter Shop
+//
+//  Created by admin on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct The_Barter_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
